09ae7386c99c1d7774147b55a67b23b0c29058ff
Ikana <louis.saffre@gmail.com>
2023-12-01 12:10:52 +0100

    Pas grand chose

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-express = require('express');
-handlebars = require('express-handlebars');
+const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
-const engine = handlebars.engine();
+const router = express.Router({ mergeParams: true });
+const port = 3010;
 
-app.engine('handlebars', engine());
+const engine = handlebars.create();
+
+app.engine('handlebars', engine.engine);
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+
+app.use(express.static('static'));
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+  
