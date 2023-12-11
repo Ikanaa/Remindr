b88388cdfabb1607cede22fc6120451e1bb1ea7c
Ikana <louis.saffre@gmail.com>
2023-12-11 15:45:42 +0100

    Ajout de la page New remind

--- index.js
+++ index.js
@@ -14,10 +14,38 @@ app.set('views', './views');
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+        reminders: [
+            {
+                title: "Mon Titre",
+                body: "le corp de mon rappel",
+                creation: "25-12-2012",
+                expiration: "30-12-2012",
+                days: "5",
+                author: "ikana",
+                groupe: "groupe_ikana",
+                color: "38, 0, 255"
+            },
+            {
+                title: "Mon Titre 2",
+                body: "le corp de mon rappel",
+                creation: "25-12-2012",
+                expiration: "30-12-2012",
+                days: "5",
+                author: "Tonic",
+                groupe: "groupe_it",
+                color: "38, 0, 20"
+            }
+        ]
+    });
+});
+
+app.get('/new', (req, res) => {
+     res.render('new_remind', {
+
+     });
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-  });
-  
+});
