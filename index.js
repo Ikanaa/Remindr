78a2072bba756ac2b16f376a47a766d6b7d2cc4a
Ikana <louis.saffre@gmail.com>
2023-12-14 17:45:31 +0100

    Ajout de la gestion utilisateur

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+//TODO Bcrypt pour le cryptage
+
 const express = require('express');
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+var session = require('express-session')
 
 const app = express();
 const router = express.Router({ mergeParams: true });
 const port = 3010;
 
+//app.set('trust proxy', 1) // trust first proxy ||  Used with secure : true
+app.use(session({
+  secret: 'Ikana Tonic',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { 
+    //necessite httpS    
+    secure: false 
+}
+}))
+
+
 const engine = handlebars.create();
+
+// For POST
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.engine('handlebars', engine.engine);
 app.set('view engine', 'handlebars');
@@ -14,6 +33,16 @@ app.set('views', './views');
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
+
+    console.log(req.session.id_user);
+
+    if (!req.session.id_user)
+    {
+        res.redirect('/login');
+        return;
+    }
+
+
     res.render('home', {
         reminders: [
             {
@@ -24,7 +53,8 @@ app.get('/', (req, res) => {
                 days: "5",
                 author: "ikana",
                 groupe: "groupe_ikana",
-                color: "38, 0, 255"
+                color: "#000000",
+                invcolor : invertColor("#000000"),
             },
             {
                 title: "Mon Titre 2",
@@ -34,13 +64,19 @@ app.get('/', (req, res) => {
                 days: "5",
                 author: "Tonic",
                 groupe: "groupe_it",
-                color: "38, 0, 20"
+                color: "#f5c211",
+                invcolor : invertColor("#f5c211"),
             }
         ]
     });
 });
 
 app.get('/new', (req, res) => {
+    if (!req.session.id_user)
+    {
+        res.redirect('/login');
+        return;
+    }
      res.render('new_remind', {
 	groups: [
 	  {name: "groupe A"},
@@ -49,6 +85,145 @@ app.get('/new', (req, res) => {
      });
 });
 
+app.post('/new', (req, res) => {
+    console.log('Got body:', req.body);
+    res.redirect("/");
+});
+
+app.get('/account', (req, res) => {
+    if (!req.session.id_user)
+    {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('account', {
+        login : "MONNOMSUPERCOOL"
+    })
+});
+
+app.post('/logout', (req, res) => {
+    req.session.regenerate(function (err) {
+  
+        if (err) next(err)
+    
+        req.session.id_user = null
+
+
+        req.session.save(function (err) {
+          if (err) return next(err)
+          res.redirect('/')
+        })
+      })
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    if (req.session.id_user)
+    {
+        res.redirect('/');
+        return;
+    }
+
+    req.session.regenerate(function (err) {
+  
+        if (err) next(err)
+    
+        // GO GET THE USER ID
+        req.session.id_user = 1
+
+
+        req.session.save(function (err) {
+          if (err) return next(err)
+          res.redirect('/')
+        })
+      })
+});
+
+app.post('/register', (req, res) => {
+    if (req.session.id_user)
+    {
+        res.redirect('/');
+        return;
+    }
+
+    if (req.body.password != req.body.repassword)
+    {
+        res.redirect('/login');
+        return;
+    }
+
+    req.session.regenerate(function (err) {
+  
+        if (err) next(err)
+    
+        // GO GET THE USER ID
+        req.session.id_user = 1
+
+
+        req.session.save(function (err) {
+          if (err) return next(err)
+          res.redirect('/')
+        })
+      })
+});
+
+
+app.get("/groups", (req,res) => {
+    
+    if (!req.session.id_user)
+    {
+        res.redirect('/');
+        return;
+    }
+
+    res.render("groups", {
+        groups : [
+            {
+                group_name : "Yes Groupe",
+                members : [
+                    {
+                        member_name : "Kackie"
+                    },
+                    {
+                        member_name : "Jack"
+                    },
+                    {
+                        member_name : "Steph"
+                    }
+                ]
+            }
+        ]
+    });
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+
+
+
+function invertColor(color) {
+    // Récupérer les valeurs RGB de la couleur
+    let r = parseInt(color.substring(1, 3), 16);
+    let g = parseInt(color.substring(3, 5), 16);
+    let b = parseInt(color.substring(5, 7), 16);
+
+    // Inverser les valeurs RGB
+    r = 255 - r;
+    g = 255 - g;
+    b = 255 - b;
+
+    // Convertir les valeurs inversées en format hexadécimal
+    let invertedColor = "#" 
+                            + r.toString(16).padStart(2, '0') 
+                            + g.toString(16).padStart(2, '0') 
+                            + b.toString(16).padStart(2, '0');
+
+    return invertedColor;
+}
